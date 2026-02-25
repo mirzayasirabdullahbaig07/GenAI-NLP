4e4da849afc88be5406801d254d9c09ac0df505a
mirzayasirabdullahbaig07 <yasirabdullah4549@gmail.com>
2026-02-25 20:21:24 +0500

    End to End NLP Pipeline

---
+++ 02_End-to-End_NLP_Pipeline/01_End-to-End_NLP_Pipeline.py
@@ -0,0 +1,22 @@
+# what is nlp pipeline?
+# nlp pipeline is a set followesd to build an end to end nlp software.
+
+# steps of pipeline
+# data acquistion
+# text preparation
+#  # text cleaning
+#  # basic preprocossing(tokenization)
+#  # adcanved preprocessing(co-refference roslition)
+# feature engingeeing
+# modeling
+#  # model building
+#  # evalution
+# deployment
+#  # deployment
+#  # monitoring
+#  # update
+
+
+# so dicuss it more clearly
+# Data Acquistion
+#
